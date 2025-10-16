5d3cd1a26c3ff4e9f8b91677a33afdf0b7ca2178
ryanhess <hess29@gmail.com>
2025-10-16 10:02:14 -0400

    i think this is done.

---
+++ Makefile
@@ -0,0 +1,6 @@
+.PHONY: make clean
+
+make:
+	npm run dev
+
+clean:
--- pages/index.js
+++ pages/index.js
@@ -18,10 +18,32 @@ function Square({ value, onSquareClick }) {
 
 function GameStatusBox({ winner, turn }) {
   if (winner) {
-    return <div>Winner: {winner}</div>
+    return <div>Winner: {winner}</div>;
   } else {
-    return <div>Current Player: {turn}</div>
+    return <div>Current Player: {turn}</div>;
   }
+}
+
+function GameHistoryDisplay({ history, onJump }) {
+  let movesList = history.map((boardState, index) => {
+    let description = '';
+    if (index == 0) {
+      description = 'New Game'
+    } else {
+      description = 'Move ' + index;
+    }
+    return (
+      <li key={index}>
+        <button onClick={() => onJump(index)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <ol className="history">
+      {movesList}
+    </ol>
+  );
 }
 
 export default function Game() {
@@ -29,107 +51,131 @@ export default function Game() {
 
   const [winner, setWinner] = useState(null);
 
-  const [currentGameState, setCurrentGameState] = useState([
+  const [currentBoardState, setCurrentBoardState] = useState([
     [null, null, null],
     [null, null, null],
     [null, null, null]
   ]);
 
-  function incrementTurn() {
+  const [gameHistory, setGameHistory] = useState([
+    {
+      turn: turn,
+      winner: winner,
+      boardState: [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
+      ]
+    }
+  ]);
+
+  function jumpTo(move) {
+    setTurn(gameHistory[move].turn)
+    setWinner(gameHistory[move].winner)
+    setCurrentBoardState(gameHistory[move].boardState)
+    setGameHistory(gameHistory.slice(0, move + 1))
+  }
+
+  function nextTurn() {
     if (turn === symbol.X) {
-      setTurn(symbol.O);
+      return symbol.O;
     } else {
-      setTurn(symbol.X);
+      return symbol.X;
     }
   }
 
+  // returns the winner given the current board state
+  // returns null if no winner.
   function calculateWinner() {
     // check all rows
     for (let row = 0; row <= 2; row++) {
       if (
-        JSON.stringify(currentGameState[row]) ===
+        JSON.stringify(currentBoardState[row]) ===
         JSON.stringify([turn, turn, turn])
       ) {
-        setWinner(turn);
-        return;
+        return turn;
       }
     }
     // check all columns
     for (let col = 0; col <= 2; col++) {
       if (
         JSON.stringify(
-          [currentGameState[0][col], currentGameState[1][col], currentGameState[2][col]]
+          [currentBoardState[0][col], currentBoardState[1][col], currentBoardState[2][col]]
         ) ===
         JSON.stringify(
           [turn, turn, turn]
         )
       ) {
-        setWinner(turn);
-        return;
+        return turn;
       }
     }
     // check for diags
     if (
-      JSON.stringify([currentGameState[0][0], currentGameState[1][1], currentGameState[2][2]]) ===
+      JSON.stringify([currentBoardState[0][0], currentBoardState[1][1], currentBoardState[2][2]]) ===
       JSON.stringify([turn, turn, turn])
     ) {
-      setWinner(turn);
-      return;
+      return turn;
     }
     if (
-      JSON.stringify([currentGameState[2][0], currentGameState[1][1], currentGameState[0][2]]) ===
+      JSON.stringify([currentBoardState[2][0], currentBoardState[1][1], currentBoardState[0][2]]) ===
       JSON.stringify([turn, turn, turn])
     ) {
-      setWinner(turn);
-      return;
+      return turn;
     }
 
-    return;
+    return null;
   }
 
   function playTurn(row, col, tur) {
     if (!winner) {
-      const nextGameState = currentGameState.slice();
-      if (currentGameState[row][col]) {
-        return
+      const nextBoardState = currentBoardState.slice();
+      const newGameHistory = structuredClone(gameHistory);
+      if (currentBoardState[row][col]) {
+        return;
       }
-      nextGameState[row][col] = tur;
-      setCurrentGameState(nextGameState);
-      calculateWinner();
-      incrementTurn();
-      console.log(turn + ' ' + winner)
+      nextBoardState[row][col] = tur;
+      setCurrentBoardState(nextBoardState);
+      setWinner(calculateWinner());
+      setTurn(nextTurn());
+      console.log
+      newGameHistory.push(
+        {
+          turn: nextTurn(),
+          winner: calculateWinner(),
+          boardState: nextBoardState
+        }
+      );
+      setGameHistory(newGameHistory);
+      console.log(newGameHistory)
     }
   }
 
   return (
     <>
       <GameStatusBox winner={winner} turn={turn} />
-      <Board turn={turn} gameState={currentGameState} winner={winner} onPlay={playTurn} />
+      <Board turn={turn} gameState={currentBoardState} winner={winner} onPlay={playTurn} />
+      <GameHistoryDisplay history={gameHistory} onJump={jumpTo} />
     </>
   );
 }
 
-function Board({ turn, gameState, winner, onPlay }) {
-  function play(row, col, tur) {
-    onPlay(row, col, tur)
-  }
-
+function Board({ turn, gameState, onPlay }) {
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={gameState[0][0]} onSquareClick={() => play(0, 0, turn)} />
-        <Square value={gameState[0][1]} onSquareClick={() => play(0, 1, turn)} />
-        <Square value={gameState[0][2]} onSquareClick={() => play(0, 2, turn)} />
+        <Square value={gameState[0][0]} onSquareClick={() => onPlay(0, 0, turn)} />
+        <Square value={gameState[0][1]} onSquareClick={() => onPlay(0, 1, turn)} />
+        <Square value={gameState[0][2]} onSquareClick={() => onPlay(0, 2, turn)} />
       </div>
       <div className="board-row">
-        <Square value={gameState[1][0]} onSquareClick={() => play(1, 0, turn)} />
-        <Square value={gameState[1][1]} onSquareClick={() => play(1, 1, turn)} />
-        <Square value={gameState[1][2]} onSquareClick={() => play(1, 2, turn)} />
+        <Square value={gameState[1][0]} onSquareClick={() => onPlay(1, 0, turn)} />
+        <Square value={gameState[1][1]} onSquareClick={() => onPlay(1, 1, turn)} />
+        <Square value={gameState[1][2]} onSquareClick={() => onPlay(1, 2, turn)} />
       </div>
       <div className="board-row">
-        <Square value={gameState[2][0]} onSquareClick={() => play(2, 0, turn)} />
-        <Square value={gameState[2][1]} onSquareClick={() => play(2, 1, turn)} />
-        <Square value={gameState[2][2]} onSquareClick={() => play(2, 2, turn)} />
+        <Square value={gameState[2][0]} onSquareClick={() => onPlay(2, 0, turn)} />
+        <Square value={gameState[2][1]} onSquareClick={() => onPlay(2, 1, turn)} />
+        <Square value={gameState[2][2]} onSquareClick={() => onPlay(2, 2, turn)} />
       </div>
     </div>
   );
